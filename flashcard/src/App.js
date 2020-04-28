e3f60bbbba8116f9c4a1dd8e8191f34ad844ccd2
Michael Spremulli <michaels.wpi@gmail.com>
2020-04-27 21:45:36 -0400

    added Schema and some css

---
+++ flashcard/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Navbar from './components/Navbar.js';
+import './App.css';
+
+
+class App extends Component {
+  render(){
+
+    return (
+      <Navbar />
+    );
+  }
+}
+
+export default App;
