e67aefe3b9a60a2fa2d5ef143400334124ababf6
Michael Spremulli <michaels.wpi@gmail.com>
2020-08-17 21:18:07 -0400

    moved questions to only display one at a time

--- client/src/components/FlashcardList.js
+++ client/src/components/FlashcardList.js
@@ -3,69 +3,72 @@ import {connect} from 'react-redux';
 import {initCards, counter} from '../actions';
 import axios from 'axios';
 
-
 //change back to class to use componentdidmount method??
 class FlashcardList extends Component  {
   
+ 
+  state = {
+    currentCard: 0
+  };
+
   //get the flashcard data from the backend
   getFlashcard = () => {
     axios.get('http://localhost:5000/cards')
     .then((response) => {
       const data = response.data;
-      // console.log('data',data);
-
-
       this.props.initCards(data);
      
-     
-      console.log('data recieved')
-      
+      console.log('data recieved');
     })
     .catch(() => {
-      console.log('data not found')
+      console.log('data not found');
     })
   }
   
   //should only run once, need to move somewhere else
-  componentDidMount(){
+  componentDidMount() {
     this.getFlashcard();
-  }
-
-  localStyle = { 
-    show: {
-      opacity: "100"
-    },
-    hide: {
-      opacity: "0"
-    }
-    
   }
 
   //shows the answer
   showAnswer = () => {
-    console.log('show answer');
+
+  }
+
+  //look at next question. loop back to begining if at end
+  nextQuestion = () => {
+    if(this.state.currentCard + 1 < this.props.cards.length){
+      this.setState({
+        currentCard: this.state.currentCard + 1
+      });
+    }
+    else{
+
+      this.setState({
+        currentCard: 0
+      });
+    }
   }
  render(){
-  const props = this.props;
- 
+   const cards = this.props.cards;
   //take the single cards and display a list of them all
   //only show the answer after it has been clicked
-    const flashCardQuestions = props.cards.map(card => {
+    const flashCardQuestions = cards.map(card => {
       return(
         <div key = {card._id}>
           <li className = "singleCard">{card.question}</li>
           <button onClick = {() => this.showAnswer()}>Show Answer</button>
-          <li className = " singleCard" style = {this.localStyle.show}>{card.answer}</li> 
-          <button>Next Question</button>
+          <li className = " singleCard" >{card.answer}</li> 
+          <button onClick = {() => this.nextQuestion()}>Next Question</button>
         </div>
       )
-    })
+    });
 
-      return (
-      <div>
-        {flashCardQuestions}
-      </div>
-      )
+    return (
+    <div>
+      {flashCardQuestions[this.state.currentCard]}
+    </div>
+    )
   }
 }
 
