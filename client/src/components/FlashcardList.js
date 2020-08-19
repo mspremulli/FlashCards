8f0a2b2e4e3a1def900ad8a3b289f2eb1cb5e657
Michael Spremulli <michaels.wpi@gmail.com>
2020-08-18 22:17:39 -0400

    converted flashcardlist from class to function

--- client/src/components/FlashcardList.js
+++ client/src/components/FlashcardList.js
@@ -1,82 +1,93 @@
-import React, {Component} from 'react';
+import React, {useState , useEffect} from 'react';
 import {connect} from 'react-redux';
-import {initCards, counter} from '../actions';
+import {initCards} from '../actions';
 import axios from 'axios';
 
-//change back to class to use componentdidmount method??
-class FlashcardList extends Component  {
-  
- 
-  state = {
-    currentCard: 0
-  };
+const FlashcardList = ({cards}) =>  {
 
-  //get the flashcard data from the backend
-  getFlashcard = () => {
-    axios.get('http://localhost:5000/cards')
-    .then((response) => {
-      const data = response.data;
-      this.props.initCards(data);
-     
-      console.log('data recieved');
-    })
-    .catch(() => {
-      console.log('data not found');
-    })
-  }
-  
-  //should only run once, need to move somewhere else
-  componentDidMount() {
-    this.getFlashcard();
-  }
+//   const [showResults, setShowResults] = React.useState(false)
+//   const onClick = () => setShowResults(true)
+//   return (
+//     <div>
+//       <input type="submit" value="Search" onClick={onClick} />
+//       { showResults ? <Results /> : null }
+//     </div>
+//   )
+// }
 
+// const Results = () => (
+//   <div id="results" className="search-results">
+//     Some Results
+//   </div>
+// )
+
+const getFlashcard = () => {
+  axios.get('http://localhost:5000/cards')
+  .then((response) => {
+    const data = response.data;
+    this.props.initCards(data);
+    
+    console.log('data recieved');
+  })
+  .catch(() => {
+    console.log('data not found');
+  })
+}
+
+const [cardIndex, setCardIndex] = useState(0);
+
+//get the flashcard data from the backend
+// const [cards, setCards] = useState(0);
+useEffect(() => {
+ getFlashcard();
+});
+
+
+  
   //shows the answer
-  showAnswer = () => {
+  const showAnswer = () => {
 
   }
 
   //look at next question. loop back to begining if at end
-  nextQuestion = () => {
-    if(this.state.currentCard + 1 < this.props.cards.length){
-      this.setState({
-        currentCard: this.state.currentCard + 1
-      });
+  const nextQuestion = () => {
+    if(cardIndex + 1 < cards.length){
+      setCardIndex(cardIndex + 1)
     }
     else{
-
-      this.setState({
-        currentCard: 0
-      });
+      setCardIndex(0);
     }
+    console.log(cardIndex);
   }
- render(){
-   const cards = this.props.cards;
-  //take the single cards and display a list of them all
-  //only show the answer after it has been clicked
-    const flashCardQuestions = cards.map(card => {
-      return(
-        <div key = {card._id}>
-          <li className = "singleCard">{card.question}</li>
-          <button onClick = {() => this.showAnswer()}>Show Answer</button>
-          <li className = " singleCard" >{card.answer}</li> 
-          <button onClick = {() => this.nextQuestion()}>Next Question</button>
-        </div>
-      )
-    });
 
-    return (
-    <div>
-      {flashCardQuestions[this.state.currentCard]}
-    </div>
-    )
-  }
+//   const cards = props.cards;
+//   //take the single cards and display a list of them all
+//   //only show the answer after it has been clicked
+//   const flashCardQuestions = cards.map(card => {
+//     return(
+//       <div key = {card._id}>
+//         <li className = "singleCard">{card.question}</li>
+//         <button onClick = {() => showAnswer()}>Show Answer</button>
+//         <li className = " singleCard" >{card.answer}</li> 
+//         <button onClick = {() => nextQuestion()}>Next Question</button>
+//       </div>
+//     )
+//   });
+
+  return (
+  <div>
+   {cardIndex}
+   {getFlashcard}
+   <button onClick = {() => nextQuestion()}>Next Question</button>
+  </div>
+  )
+  
 }
 
 const mapStoreToProps = (store) => {
   return {
     cards:store.cards.cardList,
-    count:store.cards.count
   }
 }
 
-export default connect(mapStoreToProps, {initCards, counter})(FlashcardList);
+export default connect(mapStoreToProps, {initCards})(FlashcardList);
